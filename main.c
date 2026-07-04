76d3fd9bb288b01448f22892a677a97aa7261fdc
ariwa <ariwa@LAPTOP-1ERJ280O.localdomain>
2026-07-04 16:42:31 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+
+
+int main() {
+
+    size_t buffer_size = 0;
+    ssize_t line_length;
+    char* input = NULL;
+
+    do {
+        printf("> ");
+
+        line_length = getline(&input, &buffer_size, stdin);
+
+        if(line_length != -1) {
+            if(input[0] != '\n') {
+                char* argv[5];
+                char* saveptr;
+                argv[0] = strtok_r(input, " \n", &saveptr);
+                int i = 1;
+                while (i < 4) {
+                    argv[i] = strtok_r(NULL, " \n", &saveptr);
+                    if(!argv[i]) {
+                        i++;
+                        break;
+                    }
+                    i++;
+                }
+
+                argv[i] = NULL;
+                
+                if (strtok_r(NULL, " ", &saveptr)) {
+                    printf("Error: too many tokens\n");
+                }
+            }
+        }
+    } while (line_length != -1);
+
+    free(input);
+    return 0;
+    
+}
